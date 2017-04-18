8519f9b89f1ae81da823fadd0fc140453991708a
NachoSoto <ignaciosoto90@gmail.com>
2017-04-18 15:18:28 -0700

    Added dynamic framework target to support Carthage

---
+++ WTracker-iOS/WTracker-iOS.h
@@ -0,0 +1,17 @@
+//
+//  WTracker-iOS.h
+//  WTracker-iOS
+//
+//  Created by Nacho Soto on 4/18/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WTracker-iOS.
+FOUNDATION_EXPORT double WTracker_iOSVersionNumber;
+
+//! Project version string for WTracker-iOS.
+FOUNDATION_EXPORT const unsigned char WTracker_iOSVersionString[];
+
+#import <WTracker_iOS/WTracker.h>
